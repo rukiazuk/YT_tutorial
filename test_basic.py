80d755978d08bddeba88dd4921627e06ecf46ea8
rukiazuk <vvetra27@o2.pl>
2024-07-11 11:32:26 +0200

    test

--- test_basic.py
+++ test_basic.py
@@ -1,2 +1,10 @@
 import requests
 
+ENDPOINT = "https://todo.pixegami.io/"
+
+# sanity test 
+# if ENDPOINT is responding
+
+def test_can_call_endopint():
+    response = requests.get(ENDPOINT)
+    assert response.status_code == 200
