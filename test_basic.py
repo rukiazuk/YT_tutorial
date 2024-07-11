daf41fa2ce92631c5216634c2452b6197bd38207
rukiazuk <vvetra27@o2.pl>
2024-07-11 11:32:41 +0200

    test

--- test_basic.py
+++ test_basic.py
@@ -8,3 +8,27 @@ ENDPOINT = "https://todo.pixegami.io/"
 def test_can_call_endopint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
+
+
+# create new task and check if it was created by using GET
+
+# python -m pytest -v -s to run and print
+
+def test_create_tasK():
+    payload = {
+        "content": "test content",
+        "user_id": "test user_id",
+        # "task_id": "test task_id", -- task ID generated server side, can be omitted
+        "is_done": False
+    }
+    create_task_response = requests.put(ENDPOINT + "/create-task", json=payload)
+    assert create_task_response.status_code == 200
+
+    data = create_task_response.json()
+    print(data)
+
+    task_id = data["task"]["task_id"]
+    get_task_response = requests.get(ENDPOINT + f"/get-task/{task_id}")
+    assert get_task_response.status_code == 200
+    get_task_data = get_task_response.json()
+    print(get_task_data)
