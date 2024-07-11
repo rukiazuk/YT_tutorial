51f443715cc9b1a45565b22e531e42783e5cb858
rukiazuk <vvetra27@o2.pl>
2024-07-11 12:55:50 +0200

    test

--- test_basic.py
+++ test_basic.py
@@ -29,6 +29,12 @@ def test_create_tasK():
 
     task_id = data["task"]["task_id"]
     get_task_response = requests.get(ENDPOINT + f"/get-task/{task_id}")
+
     assert get_task_response.status_code == 200
     get_task_data = get_task_response.json()
-    print(get_task_data)
+    assert get_task_data["content"] == payload["content"]
+    assert get_task_data["user_id"] == payload["user_id"]
+
+    # sanity test if user_id value is not something different
+    # assert get_task_data["user_id"] == "random user_id"
+    
